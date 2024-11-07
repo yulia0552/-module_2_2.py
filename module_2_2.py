2889d99f2560e4733eb3c7c31afe90c33bd31a99
Юлия <veberulia530@gmail.com>
2024-11-07 20:22:37 +0300

    Add files via upload

---
+++ module_2_2.py
@@ -0,0 +1,13 @@
+first = int(input("Введите три числа :"))
+second = int(input())
+third = int(input())
+if first == second and first == third and second == third :
+    print (3)
+elif first == second or first == third or second == third :
+    print (2)
+else :
+    print (0)
+
+
+
+
